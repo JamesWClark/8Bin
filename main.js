28a7d433d27a9f74c3893cd8ff0d480b07c8c752
J.W. Clark <jwclark@rockhursths.edu>
2020-09-01 22:05:43 -0500

    Add explanation

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const bits = document.getElementsByClassName('bit');
 const result = document.getElementById('decimal-value');
+const explanation = document.getElementById('explanation');
+
 let decimal = 0;
+
+function explain() {
+    let numOn = 0;
+    var exp = '';
+    for(let i = 0; i < bits.length; i++) {
+        if(bits[i].classList.contains('on')) {
+            numOn++;
+            const exponent = parseInt(bits[i].id[1]);
+            exp += Math.pow(2, exponent) + ' + ';
+        }
+    }
+    exp = exp.substring(0, exp.lastIndexOf('+'));
+    exp += ' is ' + decimal;
+    return numOn > 1 ? exp : '';
+}
 
 function toggle(element) {
     const bit = element.target;
@@ -17,6 +34,7 @@ function toggle(element) {
     }
 
     result.innerHTML = decimal;
+    explanation.innerHTML = explain();
 }
 
 for(let i = 0; i < bits.length; i++) {
